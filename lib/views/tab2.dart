ce2affbf55d250ccb7ad85b890d29a44b6e70159
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2024-09-12 23:50:01 -0600

    Diseño de pantalla principal admin

---
+++ lib/views/tab2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tab2 extends StatelessWidget{
+  const Tab2({super.key});
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Opcion 2"),),
+
+    );
+  }
+
+}
