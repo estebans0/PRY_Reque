f5ef640542d40dd913a2c0446aaefa9ffb88ca6b
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2024-09-16 16:38:49 -0600

    Se actualiza pantalla landing

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -3,73 +3,88 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'signup_screen.dart';
-// import 'manage_user_screen.dart';
+// import 'manage_user_screen.dart'; 
 
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Proyecto 1 - Requerimientos de Software'),
-        centerTitle: true,
+        title: Text(''),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const LoginScreen()),
-                );
-              },
-              child: const Text('Log In'),
+      body: Stack(
+        children: [
+          // Posiciona y centra el título
+          Positioned(
+            top: 50, // Cambia este valor para ajustar la posición vertical
+            left: 0,
+            right: 0, // Para centrar el texto horizontalmente
+            child: Center(
+              child: Text(
+                'Proyecto 1 - Requerimientos de Software',
+                style: TextStyle(
+                  fontSize: 28, 
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 42, 69, 105), // rgb(42, 69, 105)
+                ),
+                textAlign: TextAlign.center, // Centrar el texto
+              ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const SignupScreen()),
-                );
-              },
-              child: const Text('Sign Up'),
+          ),
+          // Centro de la pantalla con botones dentro de un cuadro
+          Center(
+            child: Container(
+              width: 300,
+              height: 200,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 212, 209, 184), //fromRGBO(212, 209, 184, 50),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const LoginScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 63, 119, 133), 
+                    ),
+                    child: Text('Log In', style: TextStyle(color: Color.fromARGB(255, 212, 209, 184)),),
+                  ),
+                  SizedBox(height: 20), // Espacio entre los botones
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const SignupScreen()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 63, 119, 133),
+                    ),
+                    child: Text('Sign Up', style: TextStyle(color: Color.fromARGB(255, 212, 209, 184)),),
+                  ),
+                ],
+              ),
             ),
-            
-            const SizedBox(height: 20),
-            
-          /*
-            // solo para ir probando lo de gestion de usuarios
-            // luego se agrega a la pantalla principal del admin
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ManageUsersScreen()),
-                );
-              },
-              child: const Text('Gestion de usuarios'),
-            ),
-          
-          */
-          // Para probar pagina principal admin mientras se implementa identificacion usuario tipo admin
-            // ElevatedButton(
-            //   onPressed: () {
-            //     Navigator.push(
-            //       context,
-            //       MaterialPageRoute(builder: (context) => const HomeScreenAdmin()),
-            //     );
-            //   },
-            //   child: const Text('Principal admin'),
-            // ),
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
