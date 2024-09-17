c3b738c4fcee61270083e2ecdd64ec7d88b4fc7c
Sebas3004 <sebasmumor2630@gmail.com>
2024-09-16 18:28:26 -0600

    Diseño cartera digital + compra

--- lib/views/buy_digital_currency_screen.dart
+++ lib/views/buy_digital_currency_screen.dart
@@ -18,72 +18,135 @@ class _BuyDigitalCurrencyScreenState extends State<BuyDigitalCurrencyScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Comprar Moneda Digital'),
+        automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            DropdownButtonFormField<String>(
-              decoration: const InputDecoration(
-                labelText: 'Método de pago',
-              ),
-              value: _selectedPaymentMethod,
-              onChanged: (String? newValue) {
-                setState(() {
-                  _selectedPaymentMethod = newValue;
-                });
-              },
-              //Esto es un placeholder, al final no se implementan formas de pago
-              items: ['Método 1', 'Método 2', 'Método 3'].map((String method) {
-                return DropdownMenuItem<String>(
-                  value: method,
-                  child: Text(method),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            //Campo para ingresar monto a comprar
-            TextField(
-              controller: _amountController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Cantidad',
-                prefixIcon: Icon(Icons.attach_money),
-                border: OutlineInputBorder(),
+      body: Stack(
+        children: [
+          //Posiciona y centra el título
+          Positioned(
+            top: 50, //Ajusta la posición vertical
+            left: 0,
+            right: 0, //Para centrar el texto horizontalmente
+            child: Center(
+              child: Text(
+                'Comprar Moneda Digital',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 42, 69, 105),
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final amount = double.tryParse(_amountController.text);
-                if (amount != null && amount > 0) {
-                  await _walletMethods.updateDigitalCurrency(amount);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Compra realizada con éxito'),
-                    ),
-                  );
+          ),
 
-                  //Va hacia a la pantalla de Wallet, eliminando las pantallas previas del stack
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    '/wallet',
-                    ModalRoute.withName('/home'),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Por favor ingrese una cantidad válida'),
+          //Cuadro principal
+          Center(
+            child: Container(
+              width: 360,
+              height: 250,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 212, 209, 184), //Color de fondo
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  DropdownButtonFormField<String>(
+                    decoration: const InputDecoration(
+                      labelText: 'Método de pago',
                     ),
-                  );
-                }
-              },
-              child: const Text('Confirmar'),
+                    value: _selectedPaymentMethod,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedPaymentMethod = newValue;
+                      });
+                    },
+                    items: ['Método 1', 'Método 2', 'Método 3']
+                        .map((String method) {
+                      return DropdownMenuItem<String>(
+                        value: method,
+                        child: Text(method),
+                      );
+                    }).toList(),
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                      labelText: 'Cantidad',
+                      prefixIcon: Icon(Icons.attach_money),
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final amount = double.tryParse(_amountController.text);
+                      if (amount != null && amount > 0) {
+                        await _walletMethods.updateDigitalCurrency(amount);
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text('Compra realizada con éxito'),
+                          ),
+                        );
+
+                        //Va hacia a la pantalla de Wallet, eliminando las pantallas previas del stack
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          '/wallet',
+                          ModalRoute.withName('/home'),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content:
+                                Text('Por favor ingrese una cantidad válida'),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 63, 119, 133),
+                    ),
+                    child: const Text(
+                      'Confirmar',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 212, 209, 184)),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+
+          //Botón de volver
+          Positioned(
+            top: 30,
+            left: 20,
+            height: 40,
+            width: 40,
+            child: Tooltip(
+              message: 'Volver',
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.pop(context); //Vuelve a la pantalla anterior
+                },
+                child: Icon(Icons.arrow_back),
+                backgroundColor: Color(0xFFE0E0D6),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/views/wallet_screen.dart
+++ lib/views/wallet_screen.dart
@@ -20,127 +20,204 @@ class _WalletScreenState extends State<WalletScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cartera Digital'),
+        automaticallyImplyLeading: false,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //Organiza en una fila el balance y las transacciones recientes
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                //Contenedor para el balance
-                FutureBuilder<double>(
-                  future: _walletMethods.getDigitalCurrency(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const CircularProgressIndicator(); //Muestra un cargando mientras se obtiene el balance
-                    } else if (snapshot.hasError) {
-                      return const Text('Error al cargar el balance');
-                    } else {
-                      return Container(
-                        width: 150,
-                        padding: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Balance actual',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            Text('Balance: ${snapshot.data}'),
-                          ],
-                        ),
-                      );
-                    }
-                  },
+      body: Stack(
+        children: [
+          //Posiciona y centra el título
+          Positioned(
+            top: 50, //Ajusta la posición vertical
+            left: 0,
+            right: 0, //Para centrar el texto horizontalmente
+            child: Center(
+              child: Text(
+                'Cartera Digital',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 42, 69, 105),
                 ),
-
-                //Contenedor para las transacciones recientes
-                FutureBuilder<List<Map<String, dynamic>>>(
-                  future: _walletMethods.getRecentTransactions(),
-                  builder: (context, transactionSnapshot) {
-                    if (transactionSnapshot.connectionState ==
-                        ConnectionState.waiting) {
-                      return const CircularProgressIndicator();
-                    } else if (transactionSnapshot.hasError) {
-                      return Text(
-                          'Error al cargar transacciones: ${transactionSnapshot.error}');
-                    } else if (transactionSnapshot.hasData &&
-                        transactionSnapshot.data!.isEmpty) {
-                      return const Text('No hay transacciones recientes');
-                    } else {
-                      return Container(
-                        width: 250,
-                        padding: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[300],
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text(
-                              'Transacciones recientes',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            ...transactionSnapshot.data!.map((transaction) {
-                              return Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 4.0),
-                                child: Text(
-                                  'Donaste ${transaction['amount']} al proyecto ${transaction['project_name']}',
-                                ),
-                              );
-                            }).toList(),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                ),
-              ],
+                textAlign: TextAlign.center,
+              ),
             ),
+          ),
 
-            const SizedBox(height: 40),
+          //cuadro principal
+          Center(
+            child: Container(
+              width: 440,
+              height: 340,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 213, 209, 184),
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 8,
+                    offset: Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //Contenedor para el balance
+                      FutureBuilder<double>(
+                        future: _walletMethods.getDigitalCurrency(),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator(); //Muestra un cargando mientras se obtiene el balance
+                          } else if (snapshot.hasError) {
+                            return const Text('Error al cargar el balance');
+                          } else {
+                            return Container(
+                              width: 150,
+                              height: 100,
+                              padding: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(
+                                    255, 223, 220, 198), //Fondo de balance
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  const Text(
+                                    'Balance actual',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  Text('Balance: ${snapshot.data}'),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                      ),
 
-            //Botones de accion
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  icon: const Icon(Icons.shopping_cart),
-                  onPressed: () async {
-                    final result = await Navigator.pushNamed(
-                        context, '/buy-digital-currency');
-                    if (result == true) {
-                      _refreshBalance();
-                    }
-                  },
-                ),
-                const SizedBox(width: 20),
-                IconButton(
-                  icon: const Icon(Icons.download),
-                  onPressed: () {
-                    _walletMethods.downloadDonations();
-                  },
-                ),
-              ],
+                      //Contenedor para las transacciones recientes
+                      FutureBuilder<List<Map<String, dynamic>>>(
+                        future: _walletMethods.getRecentTransactions(),
+                        builder: (context, transactionSnapshot) {
+                          if (transactionSnapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          } else if (transactionSnapshot.hasError) {
+                            return Text(
+                                'Error al cargar transacciones: ${transactionSnapshot.error}');
+                          } else if (transactionSnapshot.hasData &&
+                              transactionSnapshot.data!.isEmpty) {
+                            return const Text('No hay transacciones recientes');
+                          } else {
+                            return Container(
+                              width: 230,
+                              height: 220,
+                              padding: const EdgeInsets.all(16.0),
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 223, 220,
+                                    198), //Fondo de transacciones recientes
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Transacciones recientes',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 10),
+                                  ...transactionSnapshot.data!
+                                      .map((transaction) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 4.0),
+                                      child: Text(
+                                        'Donaste ${transaction['amount']} al proyecto ${transaction['project_name']}',
+                                      ),
+                                    );
+                                  }).toList(),
+                                ],
+                              ),
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+
+                  //Espaciado
+                  const SizedBox(height: 40),
+
+                  //Botones de accion
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          final result = await Navigator.pushNamed(
+                              context, '/buy-digital-currency');
+                          if (result == true) {
+                            _refreshBalance();
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 135, 171, 155),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: Icon(Icons.shopping_cart,
+                            color: Color.fromARGB(255, 212, 209, 184)),
+                      ),
+                      const SizedBox(width: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          _walletMethods.downloadDonations();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 135, 171, 155),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                        child: Icon(Icons.download,
+                            color: Color.fromARGB(255, 212, 209, 184)),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+
+          Positioned(
+            top: 30,
+            left: 20,
+            height: 40,
+            width: 40,
+            child: Tooltip(
+              message: 'Volver',
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.pop(context); // Vuelve a la pantalla anterior
+                },
+                child: Icon(Icons.arrow_back),
+                backgroundColor: Color(0xFFE0E0D6),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
